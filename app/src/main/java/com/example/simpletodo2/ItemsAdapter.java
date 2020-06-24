9272fbc70eea619e561a519585c805ff8749a9f1
eobbad <35933488+eobbad@users.noreply.github.com>
2020-06-23 18:13:16 -0700

    Finished Rendering Items on my TodoList

---
+++ app/src/main/java/com/example/simpletodo2/ItemsAdapter.java
@@ -0,0 +1,59 @@
+package com.example.simpletodo2;
+
+import androidx.annotation.NonNull;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+import java.util.ArrayList;
+
+//takes data at particular position and puts it in viewholder
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+
+
+    List<String> items;
+
+    public ItemsAdapter(List<String> items){
+        this.items = items;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View todoView= LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1,parent, false);
+        return new ViewHolder(todoView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        String item = items.get(position);
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return items.size();
+    }
+
+    //provides access to views that represent each row of list
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvItem;
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+        }
+
+
+        public void bind(String item) {
+            tvItem.setText(item);
+        }
+    }
+}
